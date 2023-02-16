cf71e1a26089a9d9de885fc322e1834eccc93a95
unknown <cwmitchelle@gmail.com>
2023-02-15 23:21:07 -0800

    repo

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 #This program demonstrates a gueesing game
 #game
-from random import randit
+from random import randint
 
 
 # 1 get user input  
@@ -8,19 +8,32 @@ user_name=input("whats your name?")
 print("Hello there" +user_name +"!")
 
 #generate a random number
-number=randint(10,50)
+random_number=randint(10,50)
 
 
 
 counter=0
 while counter <5:
     user_number=eval(input("Enter a random number:"))
+    counter += 1
+
+    if user_number<random_number:
+      print("Your guess is too low.")
+    elif user_number >random_number:
+      print("Your guess is too high.")
+    elif user_number==random_number:
+      print("you win!")
+    break
 
 
 
+print("Game over:")
 
-
-
+if user_number== random_number:
+    print("You win!")
+else:
+    print("Game over!The correct number is")
+    print(random_number)
 
 
 # 2 generate a random number
